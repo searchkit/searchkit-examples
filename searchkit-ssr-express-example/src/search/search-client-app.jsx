ef6cd52ff59bfe5b37eeaac987aa1ad8b530b23f
Siavash <ssetem@googlemail.com>
2017-10-10 18:24:46 +0100

    update to hydrate

--- searchkit-ssr-express-example/src/search/search-client-app.jsx
+++ searchkit-ssr-express-example/src/search/search-client-app.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import SearchApp from "./search-app.jsx"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(
+ReactDOM.hydrate(
     <SearchApp {...window.__INITIAL_APP_STATE__}/>,
     document.getElementById("searchApp")
 )
